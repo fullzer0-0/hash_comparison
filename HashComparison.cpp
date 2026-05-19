7885b73ce673359c3d880ba042a1bd5a666bc51c
fullzer0 <gnusarovvladislav@gmail.com>
2026-05-20 00:17:45 +0300

    feat: Инициализация проекта

---
+++ HashComparison.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+    printf("Приветствую!\n");
+
+    return 0;
+}
